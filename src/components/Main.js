95a757d50b4a149be94658abfd8d8ecf9a8f5554
Jsceoz <jsceoz96@gmail.com>
2016-11-19 21:16:04 +0800

    init

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,19 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AP from './account/AccountPageComponent'
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <MuiThemeProvider>
+        <div className="index">
+          <AP/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
